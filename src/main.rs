fd8e712bcd80b24548cfd29dc98e0edbe9ba461b
Marcelo Lazaroni <lazaronijunior@gmail.com>
2023-09-16 21:48:08 +0100

    Add naive evaluation loop

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,38 @@ use core::mem::size_of;
 use std::io;
 use core::fmt::Debug;
 
+fn main() -> io::Result<()> {
+    let args = std::env::args();
+    for argument in args {
+        println!("{argument}");
+    }
+
+    let board = starting_board();
+    let player = Player::White;
+    let mut best_move = None;
+    for candidate in moves(player, &board) {
+        let rating = rate(&candidate);
+        best_move = Some(match best_move.take() {
+            Some((best_board, best_rating)) => {
+                if rating > best_rating {
+                    (candidate, rating)
+                } else {
+                    (best_board, best_rating)
+                }
+            },
+            None => (candidate, rating)
+        });
+    }
+
+    println!("{}", size_of::<Piece>());
+    println!("{best_move:?}",);
+    Ok(())
+}
+
+// -----------------
+// Types
+// -----------------
+
 #[derive(Debug, PartialEq)]
 enum Character {
     Hook,
@@ -13,25 +45,43 @@ enum Character {
 }
 
 #[derive(Debug, PartialEq)]
-enum Colour {
+enum Player {
     Black,
     White
 }
 
 #[derive(Debug, PartialEq)]
-struct Piece(Colour, Character);
+struct Piece(Player, Character);
 
 type Board = [Option<Piece>; 64];
 
-fn main() -> io::Result<()> {
-    let args = std::env::args();
-    for argument in args {
-        println!("{argument}");
-    }
 
-    println!("{}", size_of::<Piece>());
-    Ok(())
+// -----------------
+// Moves
+// -----------------
+
+struct Moves<'a> {
+    board: &'a Board,
+    player: Player,
+    piece: u8,
+    move_: u64,
 }
+
+// All possible moves for a Player
+fn moves(player : Player, board : &Board) -> Vec<Board> {
+    Vec::new()
+}
+
+type Rating = u64;
+
+fn rate(board : &Board) -> Rating {
+    0
+}
+
+
+// -----------------
+// Tests
+// -----------------
 
 #[cfg(test)]
 mod chess_tests {
@@ -40,74 +90,75 @@ mod chess_tests {
    #[test]
    fn initial_board() {
        let initial_board = starting_board();
-       assert_eq!(initial_board[7], Some(Piece(Colour::White, Character::Hook)));
+       assert_eq!(initial_board[7], Some(Piece(Player::White, Character::Hook)));
    }
 
-  fn starting_board() -> Board {
-    [
-      Some(Piece(Colour::White, Character::Hook)),
-      Some(Piece(Colour::White, Character::Knight)),
-      Some(Piece(Colour::White, Character::Bishop)),
-      Some(Piece(Colour::White, Character::King)),
-      Some(Piece(Colour::White, Character::Queen)),
-      Some(Piece(Colour::White, Character::Bishop)),
-      Some(Piece(Colour::White, Character::Knight)),
-      Some(Piece(Colour::White, Character::Hook)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      None,
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Pawn)),
-      Some(Piece(Colour::White, Character::Hook)),
-      Some(Piece(Colour::White, Character::Knight)),
-      Some(Piece(Colour::White, Character::Bishop)),
-      Some(Piece(Colour::White, Character::Queen)),
-      Some(Piece(Colour::White, Character::King)),
-      Some(Piece(Colour::White, Character::Bishop)),
-      Some(Piece(Colour::White, Character::Knight)),
-      Some(Piece(Colour::White, Character::Hook))]
-  }
+}
+
+fn starting_board() -> Board {
+[
+  Some(Piece(Player::White, Character::Hook)),
+  Some(Piece(Player::White, Character::Knight)),
+  Some(Piece(Player::White, Character::Bishop)),
+  Some(Piece(Player::White, Character::King)),
+  Some(Piece(Player::White, Character::Queen)),
+  Some(Piece(Player::White, Character::Bishop)),
+  Some(Piece(Player::White, Character::Knight)),
+  Some(Piece(Player::White, Character::Hook)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  None,
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Pawn)),
+  Some(Piece(Player::White, Character::Hook)),
+  Some(Piece(Player::White, Character::Knight)),
+  Some(Piece(Player::White, Character::Bishop)),
+  Some(Piece(Player::White, Character::Queen)),
+  Some(Piece(Player::White, Character::King)),
+  Some(Piece(Player::White, Character::Bishop)),
+  Some(Piece(Player::White, Character::Knight)),
+  Some(Piece(Player::White, Character::Hook))]
 }
